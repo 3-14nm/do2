8c3aabe63419772d360ba4c5dea1e00e52a9d280
3-14nm <Greenteark800@yandex.ru>
2022-04-28 00:55:53 +0300

    Lesson #1

--- do/main/urls.py
+++ do/main/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 urlpatterns = [
 
-    path('/do2/login', views.index, name='login'),
-    path('/do2/task/<int:pk>/', views.task, name='task'),
-    path('/do2/taske', views.taske, name='taske'),
-    path('/do2/', views.home, name='home'),
-    path('/do2/account', views.account, name='account'),
+    path('login', views.index, name='login'),
+    path('task/<int:pk>/', views.task, name='task'),
+    path('taske', views.taske, name='taske'),
+    path('', views.home, name='home'),
+    path('account', views.account, name='account'),
 
 ]
